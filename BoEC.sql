4163d2450e2af1ba92fa5ca5a357f46bf4d6b297
hieplt1018 <hieplt1018@gmail.com>
2019-04-27 08:03:41 +0700

    Fix file sql

--- BoEC.sql
+++ BoEC.sql
@@ -4,41 +4,16 @@ USE BoEC2;
 
 /* ---------------------------   Clothes   -------------------------- */
 
-CREATE TABLE Suits (
-  ID             int(10) NOT NULL AUTO_INCREMENT, 
-  TypeClother    int(10), 
-  Material       varchar(255), 
-  Form           varchar(255), 
-  ClothesItemsID int(10), 
-  PRIMARY KEY (ID)) CHARACTER SET UTF8;
-
-CREATE TABLE Casuals (
-  ID             int(10) NOT NULL AUTO_INCREMENT, 
-  TypeClother    int(10), 
-  Trend          varchar(255), 
-  ClothesItemsID int(10), 
-  PRIMARY KEY (ID)) CHARACTER SET UTF8;
-
-CREATE TABLE Sports (
-  ID             int(10) NOT NULL AUTO_INCREMENT, 
-  TypeClother    int(10), 
-  Model          varchar(255), 
-  Activity       varchar(255), 
-  ClothesItemsID int(10), 
-  PRIMARY KEY (ID)) CHARACTER SET UTF8;
-
-CREATE TABLE Dress (
-  ID             int(10) NOT NULL AUTO_INCREMENT, 
-  Type           varchar(255), 
-  ClothesItemsID int(10), 
-  PRIMARY KEY (ID)) CHARACTER SET UTF8;
-
 CREATE TABLE Clothes (
-  ID          int(10) NOT NULL,
-  ItemID         int(10) NOT NULL, 
+  ID          varchar(255) NOT NULL, 
+  Name          varchar(255), 
+  Des           varchar(255), 
+  Price         float NOT NULL,
+  DiscountID int(10) NOT NULL, 
+  Image       varchar(255), 
   Gender      varchar(10),
   Size        varchar(255), 
-  CatergoryID int(10), 
+  ClothesCatergoryID int(10) NOT NULL, 
   Company     varchar(255), 
   Country     varchar(255), 
   PRIMARY KEY (ID)) CHARACTER SET UTF8;
@@ -50,70 +25,39 @@ CREATE TABLE ClothesCatergory (
 
 /* ------------------------   Electronic   --------------------------- */
 
-CREATE TABLE Refrigerator (
-  ID              int(10) NOT NULL AUTO_INCREMENT, 
-  ElectronicsID   int(10), 
-  DoorHingers     varchar(255), 
-  BatteryInclude  bit(1) NOT NULL, 
-  Capacity        varchar(255), 
-  BatteryRequired bit(1) NOT NULL, 
-  PRIMARY KEY (ID)) CHARACTER SET UTF8;
-
-CREATE TABLE Camera (
-  ID                 int(10) NOT NULL AUTO_INCREMENT, 
-  ElectronicsID      int(10), 
-  IsoRange           float NOT NULL, 
-  MegaPixel          float NOT NULL, 
-  WirelessTechnology varchar(255), 
-  PRIMARY KEY (ID)) CHARACTER SET UTF8;
-
-CREATE TABLE Tivi (
-  ID            int(10) NOT NULL AUTO_INCREMENT, 
-  ElectronicsID int(10), 
+CREATE TABLE Electronics (
+  Warranly          varchar(255), 
+  ScreenSize            float NOT NULL, 
   Resolution    varchar(255), 
   DisplayTech   varchar(255), 
   Hdr           varchar(255), 
   SoundBar      bit(1) NOT NULL, 
-  PRIMARY KEY (ID)) CHARACTER SET UTF8;
-
-CREATE TABLE Accessories (
-  ID            int(10) NOT NULL AUTO_INCREMENT, 
-  ElectronicsID int(10), 
-  DeviceOr      varchar(255), 
-  PRIMARY KEY (ID)) CHARACTER SET UTF8;
-
-CREATE TABLE Phone (
-  Model                 varchar(255), 
-  ScreenSize            float NOT NULL, 
-  StorageCapacity       int(10) NOT NULL, 
-  RAM                   int(10) NOT NULL, 
-  OS                    varchar(255), 
-  ID            int(10) NOT NULL AUTO_INCREMENT, 
-  ElectronicsID int(10) NOT NULL, 
-  PRIMARY KEY (ID)) CHARACTER SET UTF8;
-
-CREATE TABLE Computer (
-  DisplaySize           float NOT NULL, 
-  OS                    varchar(255), 
-  SystemMemory          int(10) NOT NULL, 
-  ID            int(10) NOT NULL AUTO_INCREMENT, 
-  ElectronicsID int(10) NOT NULL, 
-  PRIMARY KEY (ID)) CHARACTER SET UTF8;
-
-CREATE TABLE Electronics (
-  Warranly          varchar(255), 
   Color             varchar(255), 
   Model             varchar(255), 
   Storage           int(10), 
   RAM               int(10), 
   OS                varchar(255), 
-  SystemMemmory     int(10), 
+  SystemMemory     int(10), 
   Height            float, 
   LaptopAccessories bit(1), 
+  DoorHingers     varchar(255), 
+  BatteryInclude  bit(1) NOT NULL, 
+  Capacity        varchar(255), 
+  BatteryRequired bit(1) NOT NULL,
   Size              varchar(255), 
   EnergyUse         float,   
-  ID                int(10) NOT NULL AUTO_INCREMENT, 
-  ItemID         int(10) NOT NULL,
+  ID                varchar(255) NOT NULL AUTO_INCREMENT, 
+  Name          varchar(255), 
+  Des           varchar(255), 
+  Price         float NOT NULL,
+  DiscountID int(10) NOT NULL, 
+  Image       varchar(255), 
+  ElectronicCategory int(10) NOT NULL,
+  PRIMARY KEY (ID)) CHARACTER SET UTF8;
+
+CREATE TABLE ElectronicCategory (
+  ID   int(10) NOT NULL AUTO_INCREMENT, 
+  Name varchar(255), 
   PRIMARY KEY (ID)) CHARACTER SET UTF8;
 
 /* -------------------------   Book   ---------------------------- */
@@ -124,11 +68,15 @@ CREATE TABLE Book (
   Quantity       int(10) NOT NULL, 
   Edition        int(10) NOT NULL, 
   PublishedYear  int(10) NOT NULL,   
-  ID             int(10) NOT NULL AUTO_INCREMENT, 
-  ItemID         int(10) NOT NULL, 
+  ID             varchar(255) NOT NULL AUTO_INCREMENT, 
+  Name          varchar(255), 
+  Des           varchar(255), 
+  Price         float NOT NULL,
+  DiscountID int(10) NOT NULL, 
+  Image       varchar(255), 
   BookCategoryID int(10) NOT NULL, 
   PublisherID    int(10) NOT NULL,
-  AuthorPersonID int(10) NOT NULL,   
+  Author varchar(255) NOT NULL,   
   PRIMARY KEY (ID)) CHARACTER SET UTF8;
 
 CREATE TABLE BookCategory (
@@ -145,23 +93,7 @@ CREATE TABLE Publisher (
   Phone     varchar(255), 
   PRIMARY KEY (ID)) CHARACTER SET UTF8;
 
-CREATE TABLE Author (
-  ProfileDescription varchar(255), 
-  PersonID           int(10) NOT NULL,
-  ID                 int(10) NOT NULL AUTO_INCREMENT, 
-  PRIMARY KEY (ID)) CHARACTER SET UTF8;
-
 /* --------------------------   Item   -------------------------- */
-
-CREATE TABLE Item (
-  ID        int(10) NOT NULL AUTO_INCREMENT, 
-  StoreID  int(10) NOT NULL, 
-  Name          varchar(255), 
-  Des           varchar(255), 
-  Price         float NOT NULL,
-  DiscountID int(10) NOT NULL, 
-  Image       varchar(255), 
-  PRIMARY KEY (ID)) CHARACTER SET UTF8;
 
 CREATE TABLE Discount (
   ID        int(10) NOT NULL AUTO_INCREMENT, 
@@ -180,24 +112,9 @@ CREATE TABLE Store (
 
 /*  ----------------   Items:Cart:Orders:Bill   --------------------- */
 /* Luư trữ mặt hàng được lưu trong giỏ hàng */  
-CREATE TABLE Items (
-  ID         int(10) NOT NULL AUTO_INCREMENT, 
-  CartID     int(10) NOT NULL, 
-  ItemID     int(10) NOT NULL,
-  NumberOf   int(5) NOT NULL,
-  Cost       float NOT NULL,    
-  Desciption varchar(255),      
-  PRIMARY KEY (ID)) CHARACTER SET UTF8;
-
-CREATE TABLE Cart (
-  ID            int(10) NOT NULL AUTO_INCREMENT, 
-  CustomerID    int(10) NOT NULL,
-  NumberOfItem  int(10) NOT NULL,
-  PRIMARY KEY (ID)) CHARACTER SET UTF8;
 
 CREATE TABLE Orders (
   ID            int(10) NOT NULL AUTO_INCREMENT, 
-  CartID     int(10) NOT NULL, 
   OrdersStatus         int(10) NOT NULL, 
   ReceiveDate         date, 
   OrdersAprovingID     int(10) NOT NULL, 
@@ -206,12 +123,13 @@ CREATE TABLE Orders (
 
 CREATE TABLE Bill (
   ID             int(10) NOT NULL AUTO_INCREMENT, 
-  ShiftTimeID    int(10) NOT NULL, 
+  StaffID    int(10) NOT NULL, 
   CustomerID     int(10) NOT NULL, 
   BillStatus     int(11) NOT NULL, 
   OrdersID        int(10) NOT NULL, 
-  UpdateDate     date, 
+  CreateDate     date, 
   Cost           float NOT NULL, 
+  PaymentMethod varchar(255) NOT NULL,
   PRIMARY KEY (ID)) CHARACTER SET UTF8;
 
 CREATE TABLE CardPaymentMethod (
@@ -220,18 +138,16 @@ CREATE TABLE CardPaymentMethod (
   ExpiredDate     date, 
   Ccv             int(10) NOT NULL, 
   ID              int(10) NOT NULL AUTO_INCREMENT, 
-  AddressID       int(10) NOT NULL, 
+  AddressID       int(10), 
   WebAccountID    int(10) NOT NULL, 
   PRIMARY KEY (ID)) CHARACTER SET UTF8;
 
-CREATE TABLE OrdersAproving (
+CREATE TABLE Orders_Items (
   ID         int(10) NOT NULL AUTO_INCREMENT, 
-  StaffID    int(10) NOT NULL, 
-  Status     varchar(255), 
-  Reason     varchar(255), 
-  UpdateTime date, 
+  OrderID    int(10) NOT NULL, 
+  ItemsID     varchar(255) NOT NULL, 
+  NumberOfItems int(10), 
   PRIMARY KEY (ID)) CHARACTER SET UTF8;
-
 /* ------------------------- Customer  ----------------------- */
 
 CREATE TABLE Customer (
@@ -239,20 +155,10 @@ CREATE TABLE Customer (
   Points      int(10) NOT NULL, 
   ID          int(10) NOT NULL AUTO_INCREMENT, 
   PersonID    int(10) NOT NULL,
-  PRIMARY KEY (ID)) CHARACTER SET UTF8;
-
-CREATE TABLE CommonCustomer (
-  ID         int(10) NOT NULL AUTO_INCREMENT, 
-  CustomerID int(10), 
-  PRIMARY KEY (ID)) CHARACTER SET UTF8;
-
-CREATE TABLE VipCustomer (
-  ID          int(10) NOT NULL AUTO_INCREMENT, 
-  CustomerID  int(10), 
-  Discount    int(10), 
+  TypeCustomer varchar(255) NOT NULL,
   ExprireDate date, 
   StartDate   date, 
-  DiscountID  int(10) NOT NULL, 
+  DiscountID  int(10),
   PRIMARY KEY (ID)) CHARACTER SET UTF8;
 
 /* ------------------------- Person  ------------------------- */
@@ -261,7 +167,7 @@ CREATE TABLE Person (
   ID           int(10) NOT NULL AUTO_INCREMENT, 
   WebAccountID int(10), 
   Gender       int(11) NOT NULL, 
-  FullNameID   int(10) NOT NULL, 
+  Name   varchar(255) NOT NULL, 
   Birthday     date NOT NULL, 
   AddressID    int(10) NOT NULL, 
   PRIMARY KEY (ID)) CHARACTER SET UTF8;
@@ -271,13 +177,6 @@ CREATE TABLE WebAccount (
   Username     varchar(255), 
   Password     varchar(255), 
   CreateDate   date,
-  PRIMARY KEY (ID)) CHARACTER SET UTF8;
-
-CREATE TABLE FullName (
-  ID         int(10) NOT NULL AUTO_INCREMENT, 
-  FirstName  varchar(255), 
-  MiddleName varchar(255), 
-  LastName   varchar(255), 
   PRIMARY KEY (ID)) CHARACTER SET UTF8;
 
 CREATE TABLE Address (
@@ -295,13 +194,13 @@ CREATE TABLE Staff (
   ID          int(10) NOT NULL AUTO_INCREMENT, 
   PersonID    int(10) NOT NULL, 
   PositionID  int(10) NOT NULL, 
-  Department  varchar(255), 
+  Department  varchar(255),
+  ShiftTimeID int(10) NOT NULL, 
   PRIMARY KEY (ID)) CHARACTER SET UTF8;
 
 CREATE TABLE Position (
   ID          int(10) NOT NULL AUTO_INCREMENT, 
   Names       varchar(255), 
-  Description varchar(255), 
   PRIMARY KEY (ID)) CHARACTER SET UTF8;
 
 CREATE TABLE ShiftTime (
@@ -309,7 +208,6 @@ CREATE TABLE ShiftTime (
   Day        date NOT NULL, 
   StarTime   time(6) NOT NULL, 
   FinishTime time(6) NOT NULL, 
-  StaffID    int(10) NOT NULL, 
   PRIMARY KEY (ID)) CHARACTER SET UTF8;
 
 CREATE TABLE Staff_Salary (
